22a495b39963982f3703362e322596d5c21190a9
吴泽楷的Mac <wuzekai123@wuzekaideMacdeMacBook-Pro.local>
2026-03-10 16:17:00 +0800

    添加variables.py

---
+++ Practical4/variables.py
@@ -0,0 +1,17 @@
+a=5.08
+b=5.33
+c=5.55
+d=b-a
+e=c-b
+if d>e:
+	print("d>e Deceleraing")
+elif d<e:
+	print("d<e Accelerating")
+else:
+	print("d=e Remaning Same")
+
+X=True
+Y=False
+W=X or Y
+print(W)
+
