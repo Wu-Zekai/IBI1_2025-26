8afdaab667045489169e341fb03fe0f7a1da5168
吴泽楷的Mac <wuzekai123@wuzekaideMacdeMacBook-Pro.local>
2026-05-10 20:04:32 +0800

    variables.py adds comments

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -3,15 +3,13 @@ b=5.33
 c=5.55
 d=b-a
 e=c-b
-if d>e:
-	print("d>e Deceleraing")
-elif d<e:
-	print("d<e Accelerating")
+if d > e:
+    print("The population growth between 2004-2014 (d) was larger than 2014-2024 (e).")
 else:
-	print("d=e Remaning Same")
+    print("The population growth between 2014-2024 (e) was larger than or equal to 2004-2014 (d).")
 
 X=True
 Y=False
 W=X or Y
 print(W)
-
+print("When X is " + str(X) + " and Y is " + str(Y) + ", W (X or Y) is: " + str(W))
