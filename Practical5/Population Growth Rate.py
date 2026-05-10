83d1601ae818ed709b44d22b126c4691b1b465c5
吴泽楷的Mac <wuzekai123@wuzekaideMacdeMacBook-Pro.local>
2026-05-10 20:29:36 +0800

    Population\ Growth\ Rate.py revises sorting

--- Practical5/Population Growth Rate.py
+++ Practical5/Population Growth Rate.py
@@ -7,17 +7,17 @@ population_2020 = np.array(population_2020)
 population_2024 = np.array(population_2024)
 changes=(population_2024-population_2020)/population_2020*100
 print('Population growth rate from 2020 to 2024:' + str(changes)) 
-sorted_indices = np.argsort(changes)
+sorted_indices = np.argsort(changes)[::-1]
 sorted_countries = [countries[i] for i in sorted_indices]
 sorted_changes = [changes[i] for i in sorted_indices]
 print('Countries sorted by population growth rate:' + str(sorted_countries))
-Maxium_increase_country = sorted_countries[-1]
-Maxium_decrease_country = sorted_countries[0]
-Maxium_increase_change = sorted_changes[-1]
-Maxium_decrease_change = sorted_changes[0]
+Maxium_increase_country = sorted_countries[0]
+Maxium_decrease_country = sorted_countries[-1]
+Maxium_increase_change = sorted_changes[0]
+Maxium_decrease_change = sorted_changes[-1]
 print('Maximum increase country:' + Maxium_increase_country + ', Change:' + str(Maxium_increase_change))
 print('Maximum decrease country:' + Maxium_decrease_country + ', Change:' + str(Maxium_decrease_change))
-plt.bar(countries, changes)
+plt.bar(sorted_countries, sorted_changes)
 plt.xlabel('Countries')
 plt.ylabel('Population Growth Rate (%)')
 plt.title('Population Growth Rate from 2020 to 2024')
