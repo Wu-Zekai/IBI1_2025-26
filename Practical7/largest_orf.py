134490ab6f0d1f8b803a644431d03b9fbf49ff63
吴泽楷的Mac <wuzekai123@wuzekaideMacdeMacBook-Pro.local>
2026-05-11 09:54:23 +0800

    largest_orf.py adds robustness

--- Practical7/largest_orf.py
+++ Practical7/largest_orf.py
@@ -1,8 +1,16 @@
-import re 
+import re
+
 seq = 'AAGAUACAUGCAAGUGGUGUGUCUGUUCUGAGAGGGCCUAAAAG'
-orfs=re.findall(r'AUG(?:...)*?UAA|AUG(?:...)*?UAG|AUG(?:...)*?UGA', seq)
-longest_orf = max(orfs, key=len)
-print('The longest ORF is:', longest_orf)
-print('The length of the longest ORF is:', len(longest_orf))
+regex_pattern = r'AUG(?:...)*?(?:UAA|UAG|UGA)'
+
+orfs = re.findall(regex_pattern, seq)
+
+if orfs:
+    longest_orf = max(orfs, key=len)
+    print('The longest ORF is:', longest_orf)
+    print('The length of the longest ORF is:', len(longest_orf))
+else:
+    print('No valid ORF was found in the sequence.')
+    print('The length of the longest ORF is: 0')
 
 
